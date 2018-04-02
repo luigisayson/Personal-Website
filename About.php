79877c9922737023b5381d3f84cd3f1b757fd681
luigisayson <luigisayson@yahoo.com>
2018-04-02 23:42:11 +0000

    Made navbar collapsible and added  toggler

--- About.php
+++ About.php
@@ -3,12 +3,20 @@
     <?php include("header.html");?>
     <?php include("navigation.html");?>
     <body>
-        <p>INTRODUCTION</p>
-        I am a fourth-year student at the University of California, Irvine majoring in Computer Science and minoring in Spanish.
-        I aspire to become a Software Developer and am always open to new professional experiences. 
-        
-        I created this website to showcase my projects and achievements, while practicing my web development skills. 
-        <img src="Picture.JPG" alt="Picture of Luigi" width="300px" height = "500px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>INTRODUCTION</p>
+                    I am a fourth-year student at the University of California, Irvine majoring in Computer Science and minoring in Spanish.
+                    I aspire to become a Software Developer and am always open to new professional experiences. 
+                    
+                    I created this website to showcase my projects and achievements, while practicing my web development skills. 
+                </div>
+                <div class="col-md-6">
+                    <img src="Picture.JPG" alt="Picture of Luigi" width="300px" height = "500px">
+                </div>
+            </div>
+        </div>
     </body>
     
 </html>
--- contact.php
+++ contact.php
@@ -3,41 +3,39 @@
     <?php include("header.html");?>
     <?php include("navigation.html");?>
     <body>
-        <div class = "container mb-4">
-            <div class="page-header">
-              <h1>Contact Luigi</h1>
-            </div>
-        </div>
-        <div class="container">
-        <div class="row">
-            <div class="col">
-              E-mail: luigisayson@gmail.com<br>
-              Phone Number: (714)980-2526
-            </div>
-            <div class="col">
-                <form>
-                  <div class="form-row">
-                    <div class = "col-md-6 mb-3">
-                      <input type="text" class="form-control" id="email" placeholder="Name">
+        <div class = "container">
+          <div class="page-header mb-4">
+            <h1>Contact Luigi</h1>
+          </div>
+          <div class="row">
+              <div class="col">
+                E-mail: luigisayson@gmail.com<br>
+                Phone Number: (714)980-2526
+              </div>
+              <div class="col">
+                  <form>
+                    <div class="form-row">
+                      <div class = "col-md-6 mb-3">
+                        <input type="text" class="form-control" id="email" placeholder="Name">
+                      </div>
+                      <div class="col-md-6 mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="E-mail">
+                      </div>
                     </div>
-                    <div class="col-md-6 mb-3">
-                      <input type="email" class="form-control" id="email" placeholder="E-mail">
+                    <div class="form-group">
+                         <input type="text" class="form-control" id="phone number" placeholder="Phone Number">
                     </div>
-                  </div>
-                  <div class="form-group">
-                       <input type="text" class="form-control" id="phone number" placeholder="Phone Number">
-                  </div>
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="Subject" placeholder="Subject">
-                  </div>
-                  <div class="form-group">
-                    <textarea class="form-control" rows="3" placeholder="Message" required></textarea>
-                  </div>
-                  <div class="form-group"> 
-                    <button type="submit" class="btn btn-default">Submit</button>
-                  </div>
-                </form>            
-            </div>
+                    <div class="form-group">
+                      <input type="text" class="form-control" id="Subject" placeholder="Subject">
+                    </div>
+                    <div class="form-group">
+                      <textarea class="form-control" rows="3" placeholder="Message" required></textarea>
+                    </div>
+                    <div class="form-group"> 
+                      <button type="submit" class="btn btn-default btn-success">Submit</button>
+                    </div>
+                  </form>            
+              </div>
         </div>
     </body>
     
